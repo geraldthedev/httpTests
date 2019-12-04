1d16c0f07c9e92694dc1c3acf00537c21d622a54
Gerald Bryant <hakeem1106@gmail.com>
2019-12-04 22:00:55 +0000

    server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
+const http = require('http');
+const port = 3000;
 
+
+const server = http.createServer((req, res)=>{
+    res.end();
+});
+
+server.on('error',(err, socket)=>{
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+});
+
+server.listen(port, console.log(`listening on port ${port}`))
